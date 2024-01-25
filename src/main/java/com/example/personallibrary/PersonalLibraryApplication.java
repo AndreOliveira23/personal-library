7d9ce619f804220814ed1a2e3fdc19f7a87eac4d
Andre Oliveira <andre.oliveira0235@gmail.com>
2024-01-25 15:53:38 -0300

    Testing github integration and modify Application class

--- src/main/java/com/example/personallibrary/PersonalLibraryApplication.java
+++ src/main/java/com/example/personallibrary/PersonalLibraryApplication.java
@@ -2,9 +2,17 @@ package com.example.personallibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class PersonalLibraryApplication {
+public class PersonalLibraryApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PersonalLibraryApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalLibraryApplication.class, args);
