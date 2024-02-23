58259c66bd7de37da9ea27468d38d3041256c39b
Andre Oliveira <andre.oliveira0235@gmail.com>
2024-02-23 16:50:13 -0300

    Removing WebSecurityConfigurerAdapter (deprecated since Spring Security 5.7.0-M2) and updating in-memory authentication method

--- src/main/java/com/example/personallibrary/config/SecurityConfig.java
+++ src/main/java/com/example/personallibrary/config/SecurityConfig.java
@@ -1,21 +1,21 @@
 package com.example.personallibrary.config;
 
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+ 
+@Configuration
+public class SecurityConfig {
 
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		super.configure(auth);
-	}
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
-	}
-    
+    @Bean
+    public InMemoryUserDetailsManager manager(){
+      UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("teste")
+            .roles("USER")
+            .build();
+        return new InMemoryUserDetailsManager(user); 
+    } 
 }
